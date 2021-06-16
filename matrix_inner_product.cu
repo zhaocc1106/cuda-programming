f451362983db835d2d825a1b1fc943394b97dc79
zhaocc <450959507@qq.com>
2021-06-16 10:23:32 +0800

    Add mat inner product with shared memory in gpu.

--- matrix_inner_product.cu
+++ matrix_inner_product.cu
@@ -3,10 +3,13 @@
 //
 
 #include <iostream>
+#include <sstream>
 
 #include "cuda_start.h"
 
 #define BLOCK_SIZE 16
+
+/*-----------------------------------Mat inner product in gpu without shared memory.-----------------------------------*/
 
 // Matrices are stored in row-major order:
 // M(row, col) = *(M.elements + row * M.width + col)
@@ -33,13 +36,104 @@ __global__ void MatInnerProdKernel(Matrix a, Matrix b, Matrix c) {
   c.elements[row * c.width + col] = c_element;
 }
 
+/*-----------------------------------Mat inner product in gpu with shared memory.-----------------------------------*/
+// SubMatrices are stored in row-major order:
+// M(row, col) = *(M.elements + row * M.stride + col)
+typedef struct {
+  int width;
+  int height;
+  int stride;
+  double* elements;
+} SubMatrix;
+
+// Get a sub matrix element
+__device__ double GetElement(const SubMatrix A, int row, int col) {
+  return A.elements[row * A.stride + col];
+}
+
+// Set a sub matrix element
+__device__ void SetElement(SubMatrix A, int row, int col,
+                           double value) {
+  A.elements[row * A.stride + col] = value;
+}
+
+// Get the BLOCK_SIZExBLOCK_SIZE sub-matrix Asub of A that is
+// located col sub-matrices to the right and row sub-matrices down
+// from the upper-left corner of A
+__device__ SubMatrix GetSubMatrix(Matrix A, int row, int col) {
+  SubMatrix sub;
+  sub.width = BLOCK_SIZE;
+  sub.height = BLOCK_SIZE;
+  sub.stride = A.width;
+  sub.elements = &A.elements[A.width * BLOCK_SIZE * row + BLOCK_SIZE * col];
+  return sub;
+}
+
+// Matrix multiplication kernel called by MatMul()
+__global__ void MatInnerProdKernelWithSharedMem(Matrix A, Matrix B, Matrix C) {
+  // Block row and column
+  int block_row = blockIdx.y;
+  int block_col = blockIdx.x;
+
+  // Each thread block computes one sub-matrix Csub of C
+  SubMatrix c_sub = GetSubMatrix(C, block_row, block_col);
+
+  // Each thread computes one element of Csub
+  // by accumulating results into Cvalue
+  double c_value = 0;
+
+  // Thread row and column within Csub
+  int row = threadIdx.y;
+  int col = threadIdx.x;
+
+  // Loop over all the sub-matrices of A and B that are
+  // required to compute Csub
+  // Multiply each pair of sub-matrices together
+  // and accumulate the results
+  for (int m = 0; m < (A.width / BLOCK_SIZE); ++m) {
+
+    // Get sub-matrix Asub of A
+    SubMatrix a_sub = GetSubMatrix(A, block_row, m);
+
+    // Get sub-matrix Bsub of B
+    SubMatrix b_sub = GetSubMatrix(B, m, block_col);
+
+    // Shared memory used to store Asub and Bsub respectively
+    __shared__ double a_s[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ double b_s[BLOCK_SIZE][BLOCK_SIZE];
+
+    // Load Asub and Bsub from device memory to shared memory
+    // Each thread loads one element of each sub-matrix
+    a_s[row][col] = GetElement(a_sub, row, col);
+    b_s[row][col] = GetElement(b_sub, row, col);
+
+    // Synchronize to make sure the sub-matrices are loaded
+    // before starting the computation
+    __syncthreads();
+    // Multiply Asub and Bsub together
+    for (int e = 0; e < BLOCK_SIZE; ++e) {
+      c_value += a_s[row][e] * b_s[e][col];
+    }
+
+    // Synchronize to make sure that the preceding
+    // computation is done before loading two new
+    // sub-matrices of A and B in the next iteration
+    __syncthreads();
+  }
+
+  // Write Csub to device memory
+  // Each thread writes one element
+  SetElement(c_sub, row, col, c_value);
+}
+
 /**
  * Matrix inner product in gpu. Matrix dimensions are assumed to be multiples of BLOCK_SIZE
  * @param a: Matrix a.
  * @param b: Matrix b.
  * @param c: The matrix to save ab.
+ * @param use_shared_mem: If use shared memory.
  */
-void MatInnerProdInGpu(const Matrix& a, const Matrix& b, Matrix& c) {
+void MatInnerProdInGpu(const Matrix& a, const Matrix& b, Matrix& c, bool use_shared_mem) {
   /* Load mat a and b to gpu memory. */
   Matrix g_a = {a.width, a.height, nullptr};
   size_t size = a.width * a.height * sizeof(double);
@@ -58,7 +152,11 @@ void MatInnerProdInGpu(const Matrix& a, const Matrix& b, Matrix& c) {
   /* Invoke kernel function. */
   dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE);
   dim3 dim_grid(g_c.width / dim_block.x, g_c.height / dim_block.y);
-  MatInnerProdKernel<<<dim_grid, dim_block>>>(g_a, g_b, g_c);
+  if (use_shared_mem) {
+    MatInnerProdKernelWithSharedMem<<<dim_grid, dim_block>>>(g_a, g_b, g_c);
+  } else {
+    MatInnerProdKernel<<<dim_grid, dim_block>>>(g_a, g_b, g_c);
+  }
 
   /* Copy mat c from gpu memory into cpu memory. */
   c.width = g_c.width;
@@ -75,13 +173,15 @@ void MatInnerProdInGpu(const Matrix& a, const Matrix& b, Matrix& c) {
   CHECK(cudaFree(g_c.elements));
 }
 
+/*-----------------------------------Mat inner product in cpu.-----------------------------------*/
+
 /**
  * Matrix inner product in cpu.
  * @param a: Matrix a.
  * @param b: Matrix b.
  * @param c: The matrix to save ab.
  */
-void MatInnerProdInCpu(Matrix a, Matrix b, Matrix c) {
+void MatInnerProdInCpu(const Matrix& a, const Matrix& b, Matrix& c) {
   c.width = b.width;
   c.height = a.height;
   size_t size = c.width * c.height * sizeof(double);
@@ -139,19 +239,27 @@ int main(int argc, char** argv) {
 
   Matrix c1 = {0, 0, nullptr};
   Matrix c2 = {0, 0, nullptr};
+  Matrix c3 = {0, 0, nullptr};
 
   double gpu_start = cpuSecond(); // Mark GPU start time
-  MatInnerProdInGpu(a, b, c1);
+  MatInnerProdInGpu(a, b, c1, false);
   CHECK(cudaDeviceSynchronize());
   double gpu_time = cpuSecond() - gpu_start;
-  printf("GPU Execution Time: %f sec\n", gpu_time);
+  printf("GPU Execution Time: %f sec\n", gpu_time); // GPU Execution Time: 0.061128 sec
+
+  double gpu_use_shared_mem_start = cpuSecond(); // Mark GPU with shared memory start time
+  MatInnerProdInGpu(a, b, c2, true);
+  CHECK(cudaDeviceSynchronize());
+  double gpu_use_shared_mem_time = cpuSecond() - gpu_use_shared_mem_start;
+  printf("GPU with shared memory Execution Time: %f sec\n", gpu_use_shared_mem_time); // GPU with shared memory Execution Time: 0.011619 sec
 
   double cpu_start = cpuSecond(); // Mark CPU start time
-  MatInnerProdInCpu(a, b, c2);
+  MatInnerProdInCpu(a, b, c3);
   double cpu_time = cpuSecond() - cpu_start;
-  printf("CPU Execution Time: %f sec\n", cpu_time);
+  printf("CPU Execution Time: %f sec\n", cpu_time); // CPU Execution Time: 1.684192 sec
 
-  CheckResult(c1.elements, c2.elements, c1.width * c1.height);
+  CheckResult(c3.elements, c1.elements, c1.width * c1.height);
+  CheckResult(c3.elements, c2.elements, c2.width * c2.height);
 
   if (c1.elements) {
     free(c1.elements);
